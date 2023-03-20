67b7e70890b72eabfe04213f42295dbb67f88d51
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-20 19:45:47 +0600

    end

--- js/countries.js
+++ js/countries.js
@@ -5,20 +5,45 @@ const loadCountries = () => {
 };
 
 const displayCountries = (countries) => {
-  console.log(countries[0]);
+  console.log(countries);
   const countriesHTML = countries.map((country) => getCountryHTML(country));
   //   console.log(countriesHTML[0]);
   const container = document.getElementById("countries");
   container.innerHTML = countriesHTML.join(" ");
 };
 
-const getCountryHTML = (country) => {
+// option-2
+const getCountryHTML = ({ name, flags, area, region }) => {
   return `
     <div class="country">
-        <h2>${country.name.common}</h2>
-        <img src="${country.flags.png}">
+        <h2>${name.common}</h2>
+        <p>Area: ${area}</p>
+        <p>Continent: ${region}</p>
+        <img src="${flags.png}">
     </div>
     `;
 };
+
+// option-1
+// const getCountryHTML = (country) => {
+//     // option 1
+//     const { name, flags } = country;
+//     return `
+//       <div class="country">
+//           <h2>${name.common}</h2>
+//           <img src="${flags.png}">
+//       </div>
+//       `;
+//   };
+
+// original
+// const getCountryHTML = (country) => {
+//   return `
+//     <div class="country">
+//         <h2>${country.name.common}</h2>
+//         <img src="${country.flags.png}">
+//     </div>
+//     `;
+// };
 
 loadCountries();
